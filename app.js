5d94d691eb64fde8bac5c56adfdddda3f205f9b1
miaomiaomiao <ryougishiki23333@gmail.com>
2023-02-03 16:05:36 +0800

    初始框架

--- app.js
+++ app.js
@@ -1,21 +1,23 @@
 const express = require("express");
-const routes = require("./routes"); //新增
+const bodyParser = require("body-parser"); // 引入body-parser模块
+var path = require("path");
+const cors = require("cors");
+
+const routes = require("./routes");
+
 const app = express();
 
-app.use("/", routes); //新增
+app.use(express.static(path.join(__dirname, "public")));
 
+// 解析form表单提交的数据application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true })); 
+app.use(bodyParser.json()); // 解析json数据格式
+app.use(bodyParser.text()); //解析 text/plain 数据格式
+
+app.use(cors()); // 注入cors模块解决跨域
+
+app.use("/", routes);
 
 app.listen(4000, () => {
-  console.log("server is running");
+  console.log("server is running port");
 });
-
-var path = require('path');
-
-app.use(express.static(path.join(__dirname, 'public')));
-
-var myLogger = function (req, res, next) {
-  console.log("LOGGED");
-  next();
-};
-
-app.use(myLogger);
--- routes/login.js
+++ routes/login.js
@@ -5,7 +5,27 @@ const router = express.Router(); //模块化路由
 
 router.post("/register", loginController.register);
 
+function login_middleware(req, res, next) {
+  console.log("中间件1");
+  next(); //传递给下一步
+}
 
+function login_params(req, res, next) {
+  let { name, password } = req.query;
+  if (!name || !password) {
+    //发送消息，结束响应，不需要再调用next
+    res.json({
+      message: "参数校验失败",
+    });
+  } else {
+    next();
+  }
+}
+
+router.post("/login", (req, res, next) => {
+  console.log("req", req.body);
+  res.send("登录成功");
+});
 
 module.exports = router;
 
