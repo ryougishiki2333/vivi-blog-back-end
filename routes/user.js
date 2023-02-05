47f407db04710dfcd80ccab3311e5151f18a0a25
miaomiaomiao <ryougishiki23333@gmail.com>
2023-02-05 11:55:59 +0800

    123123

--- routes/user.js
+++ routes/user.js
@@ -7,5 +7,13 @@ router.get("/list", userController.list);
 
 router.delete("/user", userController.deleteUser);
 
+const upload = require("../util/upload");
+
+router.post("/upload", upload, (req, res, next) => {
+  // 存储后的文件信息在 req.file 中，此时文件已经存储到本地了。
+  console.log(req.file);
+  res.send("success");
+});
+
 module.exports = router;
 
