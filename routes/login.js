25ae9651af2fafcb93b7fe87bcf85b3eff67fd2e
miaomiaomiao <ryougishiki23333@gmail.com>
2023-02-14 15:16:08 +0800

    234234

--- routes/login.js
+++ routes/login.js
@@ -6,23 +6,6 @@ const router = express.Router(); //模块化路由
 
 router.post("/register", loginController.register);
 
-function login_middleware(req, res, next) {
-  console.log("中间件1");
-  next(); //传递给下一步
-}
-
-function login_params(req, res, next) {
-  let { name, password } = req.query;
-  if (!name || !password) {
-    //发送消息，结束响应，不需要再调用next
-    res.json({
-      message: "参数校验失败",
-    });
-  } else {
-    next();
-  }
-}
-
 router.post("/login", (req, res, next) => {
   let { username } = req.body;
 
